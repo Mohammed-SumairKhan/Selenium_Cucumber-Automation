395bf705b9f0295c1724a911fcb4ef3c0ce7a6b2
Mohammed-SumairKhan <sumairn1432@gmail.com>
2025-09-16 22:29:01 +0530

    Add Properties Reader

---
+++ src/main/java/utility/PropertiesReader.java
@@ -0,0 +1,29 @@
+package utility;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertiesReader {
+	Properties properties;
+	
+	public PropertiesReader() {
+		try {
+			FileInputStream fi = new FileInputStream("D:\\eclipse-workspace\\Selenium_Cucumber\\src\\main\\resources\\config.properties");
+			properties.load(fi);
+		} catch(Exception e) {
+			e.getStackTrace();
+		}
+	}
+	
+	public String getBrowserName() {
+        return properties.getProperty("browser");
+    }
+	
+	public String getUrl() {
+        return properties.getProperty("url");
+    }
+	
+	 public int getHardWait() {
+	        return Integer.parseInt(properties.getProperty("hardwait"));
+	    }
+}
