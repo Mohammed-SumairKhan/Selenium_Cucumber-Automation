1bf0739d42bb50a624cdd8f4166f2b94c77cb727
Mohammed-SumairKhan <sumairn1432@gmail.com>
2025-09-18 11:48:42 +0530

    Add BrowserStartDefinations

---
+++ src/test/java/stepDefinations/BrowserStratDefinations.java
@@ -0,0 +1,16 @@
+package stepDefinations;
+
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.en.Given;
+import utility.PropertiesReader;
+
+public class BrowserStratDefinations {
+	WebDriver driver = Hooks.driver;
+	PropertiesReader propertiesReader = new PropertiesReader();
+	
+	@Given("I Open application")
+	public void i_open_application() {
+		driver.get(propertiesReader.getUrl()); // open the url 
+	}
+}
