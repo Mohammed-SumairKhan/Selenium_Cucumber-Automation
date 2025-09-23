27556bec8cf2f7d6f465d6c8ab7eb679c539f53a
Mohammed-SumairKhan <sumairn1432@gmail.com>
2025-09-23 16:57:08 +0530

    updated

--- src/main/java/pages/SearchPage.java
+++ src/main/java/pages/SearchPage.java
@@ -1,9 +1,12 @@
 package pages;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import utility.WaitUtils;
 
 /**
  * SearchPage class represents the search functionality of the application. It
@@ -26,7 +29,10 @@ public class SearchPage {
 	// Locator for clearSearchButton
 	@FindBy(xpath = "//a[contains(@class , 'clear-search')]")
 	WebElement clearSearchButton;
-
+	
+	// locator for searchResults
+	@FindBy(xpath = "//strong")
+	WebElement searchResults;
 	/**
 	 * Constructor to initialize the SearchPage object. Initializes the web elements
 	 * using PageFactory.
@@ -37,57 +43,51 @@ public class SearchPage {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-
+	
 	/**
-	 * Performs search by name. Enters the name in the search field and clicks the
-	 * search button.
-	 * 
-	 * @param name The name to search
+	 * search based on the given searchText and 
+	 * click search button
+	 * @param searchText
 	 */
-	public void searchByName(String name) {
-		searchBar.sendKeys(name);
-		searchButton.click();
-
+	public void searchBy(String searchText) {
+		try {
+		searchBar.clear(); // clear is there is any text in search bar
+		searchBar.sendKeys(searchText); // send the searchText 
+		WaitUtils.waitForElementClickable(driver, searchButton);
+		searchButton.click(); // clicks the search button 
+		} catch(Exception e) {
+			throw new RuntimeException("Failed to perform test: " + searchText + e);
+			
+		}
 	}
-
+	
 	/**
-	 * Performs search by phone number. Enters the phone number in the search field
-	 * and clicks the search button.
-	 * 
-	 * @param phoneNumber The phone number to search
+	 *  return true is the result contains the exp_res
+	 *  else false
+	 * @param expectedText
+	 * @return
 	 */
-	public void searchByPhone(String phoneNumber) {
-		searchBar.sendKeys(phoneNumber);
-		searchButton.click();
+	public boolean isResultDisplayed(String expectedText) {
+	   return WaitUtils.isTextPresent(driver, searchResults, expectedText);
 	}
-
-	/**
-	 * Performs search by service type. Enters the service name in the search field
-	 * and clicks the search button.
-	 * 
-	 * @param service The service to search
-	 */
-	public void searchByService(String service) {
-		searchBar.sendKeys(service);
-		searchButton.click();
-	}
-
-	/**
-	 * Performs search by Task ID. Enters the Task ID in the search field and clicks
-	 * the search button.
-	 * 
-	 * @param TaskId The Task ID to search
-	 */
-	public void searchByTaskId(String TaskId) {
-		searchBar.sendKeys(TaskId);
-		searchButton.click();
-	}
-
+	
 	/**
 	 * Clicks on the Clear Search button to reset the search input and results.
 	 */
 	public void clickClearSearch() {
-		clearSearchButton.click();
+		try {
+			WaitUtils.waitForElementClickable(driver, clearSearchButton);
+			clearSearchButton.click(); // click clear search button
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to click clear search button: "+ e);
+		}
 	}
-
+	/**
+	 * check is the search bar is empty or not after clicking the clear search button 
+	 * and return true if it is empty else false
+	 * @return
+	 */
+	public boolean isSearchBarEmpty() {
+		return searchBar.getAttribute("value").isEmpty();
+	}
 }
